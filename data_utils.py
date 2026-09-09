5eeeb62eb199688eb36099923f443958ab067aba
Justin Le Louedec <justinlelouedec@Justins.local>
2026-09-09 15:22:21 +0200

    added donki creation of CMEs

--- data_utils.py
+++ data_utils.py
@@ -1,10 +1,32 @@
 import os 
 import datetime
 import requests
+import json 
+import ELEvo
 
+def load_donki_cmes(path_json):
+    # Opening JSON file
+    with open(path_json) as json_file:
+        donki_data = json.load(json_file)
 
+    CMEs = []
+    for index, row in enumerate(donki_data):
+        for analysis in row['cmeAnalyses']:
+            cme = ELEvo.CME(analysis['halfAngle'],
+                            analysis['longitude'],
+                            analysis['latitude'],
+                            0.0,
+                            0.7,
+                            analysis['speed'],
+                            analysis['time21_5'],
+                            21.5,
+                            analysis['featureCode'],
+                            'DONKI'
+                            )
+            CMEs.append(cme)
+    return CMEs
 
-def get_donki_cmes(date_start,date_end,data_type='CME',file_path='data/donki_data.json'):
+def download_donki_cmes(date_start,date_end,data_type='CME',file_path='data/donki_data.json'):
     """
     Download DONKI data for the specified data type and save it as a JSON file.
 
@@ -29,4 +51,5 @@ def get_donki_cmes(date_start,date_end,data_type='CME',file_path='data/donki_dat
 
 
 if __name__ == "__main__":
-    get_donki_cmes(datetime.datetime(2026,1,12),datetime.datetime(2026,1,17))
+    download_donki_cmes(datetime.datetime(2026,1,12),datetime.datetime(2026,1,17))
+    print(len(load_donki_cmes('data/donki_data.json')))
